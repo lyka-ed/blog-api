06e2538e7b5dedc3b85105d6bd23abe49928a8d6
lyka-ed <lykaedem11@gmail.com>
2025-03-12 01:26:43 +0100

    feat: get-all-blogs

--- src/Blog/blog.services.js
+++ src/Blog/blog.services.js
@@ -1,15 +1,31 @@
-import Blog from "./__def__/blog.models.js"
+import Blog from "./__def__/blog.models.js";
 
 export const createBlogService = async (userId, data) => {
-    try {
-      const newBlog = await new Blog({ ...data, author: userId }).populate("author");
-      await newBlog.save();
-      return newBlog;
-    } catch (error) {
-      if (error.code === 11000) {
-        throw new Error("Blog already exists");
-      } else {
-        throw new Error(error.message);
-      }
+  try {
+    const newBlog = await new Blog({ ...data, author: userId }).populate(
+      "author"
+    );
+    await newBlog.save();
+    return newBlog;
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new Error("Blog already exists");
+    } else {
+      throw new Error(error.message);
     }
-  };
+  }
+};
+
+export const getAllBlogService = async ({
+  order = "desc", 
+  order_by = "createdAt", 
+  page = 1,
+  limit = 10, 
+  state,
+  authorId
+}) => {
+  try {
+    const query = {}
+    if()
+  }
+}
